1d0029f8194b78bed78c7218fc46c045c9630fdb
RishavG96 <gupta.rishav2608@gmail.com>
2023-05-25 20:53:12 +0530

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Rishav Gupta on 25/05/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
